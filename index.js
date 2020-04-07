e280eddc8cf1f4101601c3fbec8190ada1fc8a7a
Win's PA <winnclouds@gmail.com>
2020-04-07 17:30:29 +0700

    Connected database by mongoose

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express'),
       dotenv = require('dotenv'),
-      morgan = require('morgan');
-
-// Router files
-const bootcamps = require('./routes/bootcamps');
+      morgan = require('morgan'),
+      connectDB = require('./config/db');
 
 // Load env vars;
 dotenv.config({path: './config/config.env'});
+
+// Connect to database;
+connectDB();
+
+// Router files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -20,7 +24,15 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+
+// Handle unhandle promise rejections
+process.on('unhandleRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
