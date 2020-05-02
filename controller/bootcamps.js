efc646877b7693cb03ad6ee283ed3b42fe183cae
WinEfy <thangphan@efyworld.com>
2020-05-02 11:50:16 +0700

    Bootcamp delete with pre Courses

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     let query;
 
     // Copy req.query
-    const reqQuery = { ...req.query}
+    const reqQuery = { ...req.query};
 
     // Fields to exclude
     const  removeFields = ['select', 'sort', 'page', 'limit'];
@@ -26,7 +26,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
     // Finding resource
-    query = Bootcamp.find(JSON.parse(queryStr));
+    query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
     // Select Fields
     if (req.query.select) {
@@ -128,6 +128,8 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
+
+    bootcamp.remove();
 
     res.status(200).json({success: true, data: bootcamp});
 });
