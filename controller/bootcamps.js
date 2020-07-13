e9435737b0500c88f2c7f355e1faba86791b356e
WinEfy <thangphan@efyworld.com>
2020-07-13 23:49:13 +0700

    Owner post control

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -54,14 +54,21 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @access      Private
 
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
+    let bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
+
+    // Make sure user is bootcamp owner
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User $(req.params.id) is not authorized to update this bootcamp`, 404));
+    }
+
+    bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+       new: true,
+        runValidators: true
+    });
 
     res.status(200).json({success: true, data: bootcamp});
 });
@@ -75,6 +82,11 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+    }
+
+    // Make sure user is bootcamp owner
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User $(req.params.id) is not authorized to delete this bootcamp`, 404));
     }
 
     bootcamp.remove();
@@ -119,6 +131,11 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
 
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+    }
+
+    // Make sure user is bootcamp owner
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User $(req.params.id) is not authorized to update this bootcamp`, 404));
     }
 
     if (!req.files) {
