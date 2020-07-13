0cf5c1b4609b7a77a98713d39292ac6a8263dc1e
Win's Mac Mini <winnclouds@gmail.com>
2020-07-13 23:05:07 +0700

    User relationship with post Automatic destroy or insert data user

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -30,11 +30,23 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @access      Private
 
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    // Add user to req, body
+    req.body.user = req.user.id;
+
+    // Check for published bootcamp
+    const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+    // If the user is not ad admin, they can only addd one bootcamp
+    if (publishedBootcamp && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`The user with ID ${req.user.id} has already published a bootcamp`, 400));
+    }
+
     const bootcamp = await Bootcamp.create(req.body);
 
-    res
-        .status(201)
-        .json({success: true, data: bootcamp});
+    res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
 });
 
 // @desc        Update bootcamp
--- seeder.js
+++ seeder.js
@@ -8,7 +8,8 @@ dotenv.config({path: './config/config.env'});
 
 // Load models
 const Bootcamp = require('./models/Bootcamps'),
-    Course = require('./models/Course');
+    Course = require('./models/Course'),
+    User = require('./models/User');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -27,10 +28,15 @@ const courses = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/courses.json`), 'utf-8'
 );
 
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`), 'utf-8'
+);
+
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
         console.log('Data Imported ...'.green.inverse);
         process.exit();
     } catch (e) {
@@ -42,6 +48,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
         console.log('Data Destroyed ...'.red.inverse);
         process.exit();
     } catch (e) {
