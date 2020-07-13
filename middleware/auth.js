989ccb07afebf7a5ecb08471f264baaf786e7b26
Win's Mac Mini <winnclouds@gmail.com>
2020-07-13 22:34:17 +0700

    Storing by token

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ exports.protect = asyncHanlder(async (req, res, next) => {
     let token;
 
     if (req.headers.authorization &&
-        req.headers.authorization.startsWith('Win')
+        req.headers.authorization.startsWith('Bearer')
     ) {
         token = req.headers.authorization.split(' ')[1];
     }
@@ -35,3 +35,13 @@ exports.protect = asyncHanlder(async (req, res, next) => {
         return next(new ErrorResponse('Not authorize to access this routes', 401));
     }
 });
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorResponse(`User role ${req.user.role} is not unauthorized to access this route `, 403));
+        }
+        next();
+    }
+};
--- routes/courses.js
+++ routes/courses.js
@@ -10,7 +10,7 @@ const express = require('express'),
     advancedResults = require('../middleware/advancedResults'),
     router = express.Router({mergeParams: true});
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 router.route('/')
     .get(
@@ -20,11 +20,11 @@ router.route('/')
         }),
         getCourses
     )
-    .post(protect, addCourse);
+    .post(protect, authorize('publisher', 'admin'), addCourse);
 router
     .route('/:id')
     .get(getCourse)
-    .put(protect, updateCourse)
-    .delete(protect, deleteCourse);
+    .put(protect, authorize('publisher', 'admin'), updateCourse)
+    .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
