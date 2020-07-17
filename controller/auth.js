cd693ef02c09db5d14f7e27465fb614578f17fa2
WinEfy <thangphan@efyworld.com>
2020-07-17 23:25:52 +0700

    Sent mail

--- controller/auth.js
+++ controller/auth.js
@@ -101,6 +101,32 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
     await user.save({ validateBeforeSave: false });
 
+    // Create reset url
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+
+    const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+    try {
+        await sendMail({
+            email: user.email,
+            subject: 'Password reset token',
+            message
+        });
+
+        res.status(200).json({
+            success: true,
+            data: 'Email sent'
+        });
+    } catch (e) {
+        console.log(e);
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorResponse('Email could not be sent', 500));
+    }
+
     res.status(200).json({
         success: true,
         data: user
